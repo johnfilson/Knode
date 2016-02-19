cafe4b4d3672f74407e3b4472e516a9b1e5cf475
johnfilson <john_filson@hotmail.co.uk>
2016-02-19 00:28:58 +0000

    Messager-1: Added the camera icon to my Menus and enable user to accesses their camera

--- app/src/main/java/com/fyp/knode/KnodeConstants/Constants.java
+++ app/src/main/java/com/fyp/knode/KnodeConstants/Constants.java
@@ -9,4 +9,13 @@ public final class Constants {
     public static final String KEY_CONTACT_RELATION ="contactRelation";
 
     public static final int KEY_USER_LIMIT = 1000;
+
+    //For the camera Dialog
+    public static final int KEY_TAKE_PICTURE_RESPONSE = 0;
+    public static final int KEY_TAKE_VIDEO_RESPONES = 1;
+    public static final int KEY_PICK_PICTURE = 2;
+    public static final int KEY_PICK_VIDEO = 3;
+
+
+
 }
--- app/src/main/java/com/fyp/knode/ui/Messager.java
+++ app/src/main/java/com/fyp/knode/ui/Messager.java
@@ -1,5 +1,11 @@
 package com.fyp.knode.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -9,8 +15,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.fyp.knode.Adapter.SectionsPagerAdapter;
+import com.fyp.knode.EditContactsActivity;
+import com.fyp.knode.KnodeConstants.Constants;
 import com.fyp.knode.R;
 import com.fyp.knode.SlidingTabStrip.SlidingTabLayout;
 import com.parse.ParseUser;
@@ -27,9 +36,55 @@ public class Messager extends AppCompatActivity {
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
-    CharSequence mTitles[]={"Messager","My Contacts"};
-    int mNumberOfLabs =2;
+    CharSequence mTitles[] = {"Messager", "My Contacts"};
+    int mNumberOfLabs = 2;
     SlidingTabLayout mTabs;
+
+    public static final int MEDIA_TYPE_IMAGE = 4;
+    public static final int MEDIA_TYPE_VIDEO = 5;
+
+    protected Uri mMediaUri;
+
+    protected DialogInterface.OnClickListener mDialogListener =
+            new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch (which) {
+                        case 0:
+                            Intent takepicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            mMediaUri.getOutputMediaUri(MEDIA_TYPE_IMAGE);
+                            if(mMediaUri == null){
+                                Toast.makeText(Messager.this, R.string.error_with_external_storage, Toast.LENGTH_LONG).show();
+                            }else {
+                                takepicture.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                                startActivityForResult(takepicture, Constants.KEY_TAKE_PICTURE_RESPONSE);
+                            }
+                            break;
+                        case 1:
+
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            ;
+                    }
+                }
+                private Uri getOutputMediaUri (int MediaTypeImage){
+                    if(isExternalStorageAvailable()){
+                        return null;
+                    }else {
+                        return null;
+                    }
+                }
+
+                private Boolean isExternalStorageAvailable(){
+                    String state = Environment.getExternalStorageState();
+
+                    if(state.equals((Environment.MEDIA_MOUNTED))){
+                        return true;
+                    }else{return false;}
+                }
+            };
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -37,7 +92,7 @@ public class Messager extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbox);
 
@@ -58,13 +113,6 @@ public class Messager extends AppCompatActivity {
         mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
         //making the tabs even
 
-       /* mTabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
-            @Override
-            public int getIndicatorColor(int position) {
-                return getResources().getColor( R.color.tabScrllColr, i);
-            }
-        });*/
-
         mTabs.setViewPager(mViewPager);
     }
 
@@ -84,14 +132,33 @@ public class Messager extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_logout){
-            ParseUser.logOut();
-           //TODO navigateToLogIn();
-        }
+        switch (id) {
+            case R.id.action_logout :
+                ParseUser.logOut();
+                navigateToLogIn();
+                break;
+            case R.id.action_add_contacts:
+                Intent intent = new Intent(this, EditContactsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.action_camera:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setItems(R.array.camera_choices, mDialogListener);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
 
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
+
+    private void navigateToLogIn() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
 }
 
